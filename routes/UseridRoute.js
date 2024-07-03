e10e4ed1999ae20d930684023c8c8b1fbe197c2e
Prajaktasangolkar <109715943+Prajaktasangolkar@users.noreply.github.com>
2024-07-03 20:50:38 +0530

    8th

--- routes/UseridRoute.js
+++ routes/UseridRoute.js
@@ -1,13 +1,14 @@
 const express=require('express')
 const router=express.Router()
 const {getAllUser,getUser,updateUser, deleteUser,followUser,unfollowUser}=require('../Controller/UserController.js')
+const authMiddleWare = require('../Middleware/authMiddleware.js')
 
 router.get("/",getAllUser)
 router.get('/:id',getUser)
-router.put('/:id',updateUser)
-router.delete("/:id",deleteUser)
-router.put('/:id/follow',followUser)
-router.put("/:id/unfollow",unfollowUser)
+router.put('/:id', authMiddleWare, updateUser)
+router.delete("/:id",authMiddleWare,deleteUser)
+router.put('/:id/follow',authMiddleWare,followUser)
+router.put("/:id/unfollow",authMiddleWare,unfollowUser)
 
 
 module.exports=router;
